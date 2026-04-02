52df5d6a09f6fb6b6adfad050972722c7a80fc93
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-02 20:18:50 +0900

    init

---
+++ src/test/java/com/example/gigacoffee/GigaCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gigacoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
