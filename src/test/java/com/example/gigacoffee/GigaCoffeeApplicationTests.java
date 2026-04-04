5caaede0ca62417a3e6ee72aefcddb558538c77d
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-04 22:04:37 +0900

    test : contextLoads() 테스트 오류 수정

--- src/test/java/com/example/gigacoffee/GigaCoffeeApplicationTests.java
+++ src/test/java/com/example/gigacoffee/GigaCoffeeApplicationTests.java
@@ -1,10 +1,17 @@
 package com.example.gigacoffee;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GigaCoffeeApplicationTests {
+
+    @MockitoBean
+    RedissonClient redissonClient;
 
     @Test
     void contextLoads() {
