c2dc097bd9a0f75543f17140addbdbcd02baf3db
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-05 23:30:22 +0900

    chore : 테스트용 유저 변경 및 포인트 상향 조정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,6 @@
 -- 비밀번호: password (BCrypt, cost=10)
 INSERT INTO users (email, password, name, role, deleted, created_at, updated_at)
 VALUES
-    ('user@test.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', '일반유저', 'USER',  false, NOW(), NOW()),
-    ('admin@test.com', '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', '관리자',   'ADMIN', false, NOW(), NOW()),
     ('julia@email.com', '$2a$10$yTYEksECkIGAyy84Lm39YO962YVMcZ6uMhjhK5JXchlcKcUEQG3ke', 'Julia', 'USER', false, NOW(), NOW()),
     ('paul@email.com', '$2a$10$yTYEksECkIGAyy84Lm39YO962YVMcZ6uMhjhK5JXchlcKcUEQG3ke', 'Paul', 'ADMIN', false, NOW(), NOW());
 
@@ -18,4 +16,5 @@ VALUES
 
 -- 테스트용 유저 포인트 등록(userId = 1)
 INSERT INTO user_points (user_id, point_balance, version, created_at, updated_at)
-VALUES (1, 100000, 0, NOW(), NOW());
+VALUES (1, 300000, 0, NOW(), NOW()),
+VALUES (2, 300000, 0, NOW(), NOW());
